9bb6d7643b76701308b4479e06aa7b663b243a22
jtay <hi@jaydon.us>
2019-02-01 16:11:24 +0000

    Maybe this will fix live?

--- class/shufflisten.class.php
+++ class/shufflisten.class.php
@@ -19,6 +19,13 @@
 
 			$this->api = new SpotifyWebAPI\SpotifyWebAPI();
 
+			if($this->isUserLoggedIn()){
+				$this->user->refreshAccessToken($_SESSION['user_ref_token']);
+				$_SESSION['user_token'] = $this->user->getAccessToken();
+				$_SESSION['user_ref_token'] = $this->user->getRefreshToken();
+				$this->api->setAccessToken($_SESSION['user_token']);
+			}
+
 		}
 
 		public function getAuthURL(){
@@ -45,15 +52,22 @@
 		public function authorize($code){
 			if($this->user->requestAccessToken($code)){
 				$token = $this->user->getAccessToken();
+				$ref_token = $this->user->getRefreshToken();
 				$this->api->setAccessToken($token);
 
 				$user = $this->api->me();
+
+				$user = json_decode(json_encode($user), true);
 
 				$_SESSION['user_info'] = $user;
 
 				$_SESSION['user_token'] = $token;
 
+				$_SESSION['user_ref_token'] = $ref_token;
+
 				$_SESSION['user_login_status'] = true;
+
+				return true;
 
 			}else{
 				return false;	
--- public/index.php
+++ public/index.php
@@ -17,4 +17,41 @@
 		die();
 	}
 
-?>
+	?>
+	<h1 class="ui header">
+		Welcome to Shufflisten
+		<div class="sub header">
+			Logged in as <?= $_SESSION['user_info']['display_name']; ?> <a href="/logout.php" style="font-size: 0.6em;">Not you?</a>
+		</div>
+	</h1>
+	<div class="ui four stackable cards">
+		<?php
+
+		$pl = json_decode(json_encode($sl->api->getUserPlaylists($_SESSION['user_info']['id'], ['limit' => 50])), true);
+		$pl = $pl['items'];
+
+		foreach($pl as $x){
+			?>
+			<div class="card">
+				<a class="image" href="/shuffler.php?playlist=<?= $x['id']; ?>">
+					<img src="<?= $x['images'][0]['url']; ?>">
+				</a>
+				<div class="content">
+					<a class="header" href="/shuffler.php?playlist=<?= $x['id']; ?>">
+						<?= $x['name']; ?>
+					</a>
+					<div class="meta">
+						<a class="ui green label" href="<?= $x['owner']['external_urls']['spotify']; ?>">
+							<i class="user icon"></i>&nbsp;&nbsp;<?= $x['owner']['display_name']; ?>
+						</a>
+						<span class="ui blue label">
+							<i class="hashtag icon"></i>&nbsp;&nbsp;<?= number_format($x['tracks']['total']); ?> tracks
+						</span>						
+					</div>
+				</div>
+			</div>
+			<?php
+		}
+
+		?>
+	</div>
